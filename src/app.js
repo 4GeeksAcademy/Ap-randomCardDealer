72354526cfe424cd95430d51f16569db9ded7778
Adrian <adrian.pina11@gmail.com>
2024-08-12 14:43:55 +0000

    Random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const suits = ["♥", "♦", "♣", "♠"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateRandomCard() {
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("top-left-suit").innerText = randomSuit;
+  document.getElementById("bottom-right-suit").innerText = randomSuit;
+  document.getElementById("middle-value").innerText = randomValue;
+
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    document.getElementById("top-left-suit").style.color = "red";
+    document.getElementById("bottom-right-suit").style.color = "red";
+    document.getElementById("middle-value").style.color = "red";
+  } else {
+    document.getElementById("top-left-suit").style.color = "black";
+    document.getElementById("bottom-right-suit").style.color = "black";
+    document.getElementById("middle-value").style.color = "black";
+  }
+}
+
+window.onload = generateRandomCard;
